3bedb0c1f1b87120955052000062416c67b9fd94
Himayat <himayatu99@gmail.com>
2024-11-25 23:08:42 +0500

    complete contact page

--- selectors/contact.spec.js
+++ selectors/contact.spec.js
@@ -3,5 +3,14 @@ export const contactpage = {
       url: 'https://demoblaze.com',
       navbarhome: '.nav-item.active>a',
       Contact: '[data-target="#exampleModal"]',
+      newmessagetext: '#exampleModalLabel',
+      closex: '.modal-header>button',
+      Contactemaillable: '[for="recipient-name"]',
+      contactemailinput: '#recipient-email',
+      contactnameinput: '#recipient-name',
+      messagetext: '[for="message-text"]',
+      messagetextinput: '#message-text',
+      sendmessagebtn: 'button',
+
 
 }
--- tests/Contact.spec.js
+++ tests/Contact.spec.js
@@ -1,6 +1,5 @@
 const { test, expect } = require('@playwright/test');
 const { contactpage } = require('../selectors/contact.spec');
-const { uptime } = require('process');
 
 test('Contact', async ({ page }) => {
 
@@ -13,15 +12,56 @@ test('Contact', async ({ page }) => {
 
       //verify pop-up
       // close pop-up
-      const closeX = page.locator('.modal-header>button').first();
+      const closeX = page.locator(contactpage.closex).first();
       await closeX.click()
 
       const contactagain = await page.locator(contactpage.Contact)
       await contactagain.click()
       //New message 
-      const newmessagetext = await page.locator('#exampleModalLabel');
+      const newmessagetext = await page.locator(contactpage.newmessagetext);
       await expect(newmessagetext).toHaveText('New message');
 
+      // Contact Email:
+      const contactemail = await page.locator(contactpage.Contactemaillable, {
+            hasText: 'Contact Name:'
+      })
+      await expect(contactemail).toHaveText('Contact Name:')
+
+      //Contact Email input 
+      const contactemailinput = await page.locator(contactpage.contactemailinput)
+      await contactemailinput.type('johndoe@test.com');
+
+      // Contact Name:
+      const contactname = await page.locator(contactpage.Contactemaillable, {
+            hasText: 'Contact Name:'
+      })
+      await expect(contactname).toHaveText('Contact Name:')
+
+      //Contact Name input 
+      const contactnameinput = await page.locator(contactpage.contactnameinput)
+      await contactnameinput.type('JohnDoe');
+
+      // Message:
+      const message = await page.locator(contactpage.messagetext)
+      await expect(message).toHaveText('Message:')
+
+      // Message input 
+      const messageinput = await page.locator(contactpage.messagetextinput)
+      await messageinput.type('This is testing site');
+
+      //Send message 
+      const sendmessagebtton = await page.locator(contactpage.sendmessagebtn, {
+            hasText: 'Send message'
+      });
+      await sendmessagebtton.click();
+      // Listen for the alert dialog
+      page.on('dialog', async (dialog) => {
+            // Verify the alert message
+            expect(dialog.message()).toBe('Thanks for the message!!');
+
+            // Accept the alert
+            await dialog.accept();
+      });
 
 
 
